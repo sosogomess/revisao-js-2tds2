5b0d095f04582e409aef2dfa117c67b420aaaa5c
Sophia Gomes <sophiagomes.tds2.senai@gmail.com>
2024-08-08 16:57:18 -0300

    feat: soluçao do exercicio das armas

--- script05.js
+++ script05.js
@@ -6,7 +6,7 @@ let vegetais = ["Tomate", "Cebola", "Pimentão", "Azeitona", "Manjericão"];
 
 let pizza = [];
 
-pizza.push(massas[1]);
+pizza.push(massas[0]);
 pizza.push(molhos[0]);
 pizza.push(queijos[2]);
 pizza.push(carnes[3]);
@@ -15,5 +15,31 @@ pizza.push(vegetais[4]);
 console.log("Pizza montada:");
 
 for (let i = 0; i < pizza.length; i++) {
+   if(i == 0 && pizza[i]== massas[1]){
+    console.log("hmmm, ta fit");
+   }
     console.log(pizza[i]);
 }
+
+
+let armas = ["sniper", "fuzil", "AK47", "misseis", "laser", "chicotada" ];
+let ataques = [];
+
+ataques.push("dev so");
+ataques.push(armas[5]);
+ataques.push(armas[0]);
+ataques.push(armas[3]);
+ataques.push(armas[5]);
+
+// console.log("Nome do Robô:" +ataques[0]);
+
+    for (let i = 0; i < ataques.length; i++) {
+        if (i == 0) {
+           //  console.log('Nome do robô: &{ataques[i]}');
+           console.log("Nome do robô: " + ataques[i]);
+
+        }else {
+           // console.log('ataques & {i}: &{ataques[1]}');
+           console.log("Ataque "+ i + ":" + ataques[i]);
+        }
+    }
